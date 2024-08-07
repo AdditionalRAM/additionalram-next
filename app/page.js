8e711a6d3fef672b2abab6306340fb701d402a1e
AdditionalRAM <taha@additionalram.com>
2024-08-07 16:35:17 +0300

    Made the icons icon

--- app/page.js
+++ app/page.js
@@ -3,21 +3,27 @@ import styles from "./page.module.css";
 import ThreeCRT from "./components/ThreeCRT";
 import RotatingIcons from "./components/RotatingIcons";
 
-let elementsToRotate = [
-  <p>X</p>,
-  <p>A</p>,
-  <p>B</p>,
-  <p>C</p>,
-  <p>D</p>,
-  <p>E</p>,
+async function fetchExternalLinks() {
+  // Fetch the data from the JSON file
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/external-links.json`);  const data = await response.json();
+  return data; // Assuming the data is an array of icons or similar
+}
 
-];
+export default async function Home() {
 
-export default function Home() {
+  // await externalLinks and parse JSON into array
+  const externalLinks = await fetchExternalLinks();
+
+  const smIcons = externalLinks.map((link) => (
+    <a href={link.link} key={link.name} className={styles.socialLink} target="_blank">
+      <img alt={link.name} src={link.iconURL} />
+    </a>
+  ));
+
   return (
     <main>
       <ThreeCRT elementID="threecrt" />
-      <RotatingIcons elements={elementsToRotate} centerSelector="#threecrt" radius={200} speed={10} />
+      <RotatingIcons elements={smIcons} centerSelector="#threecrt" radius={200} speed={10} />
     </main>
   );
 }
