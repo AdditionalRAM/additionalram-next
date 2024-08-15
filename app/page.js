263f32949f6d938b1ccaad30481f0752d5ceff4f
AdditionalRAM <taha@additionalram.com>
2024-08-15 12:50:39 +0300

    Added game stack

--- app/page.js
+++ app/page.js
@@ -40,6 +40,14 @@ export default async function Home() {
 
   const webProjects = await fetchLocalJson("web-projects");
 
+  const gameStack = await fetchLocalJson("game-stack");
+
+  const gameStackIcons = gameStack.map((data, index) => (
+    <div id={`${data.title}-${index}-holder`} className={styles.socialLink}>
+      <TravellingIcon key={data.title} iconURL={data.imageURL} iconID={`${data.title}-icon`} shouldInvert={data.invertLogo} travelFromID={`${data.title}-${index}-holder`} travelToID={`${data.title}-stack-member-icon`} travelDuration={0.7} imgAlt={data.title} />
+    </div>
+  ));
+
   return (
     <main>
       <AccessibleHeading text="AdditionalRAM's Portfolio" level={1} />
@@ -47,7 +55,7 @@ export default async function Home() {
         <ThreeCRT elementID="threecrt" />
         <RotatingIcons elements={smIcons} centerSelector="#threecrt" radiusVW={20} speed={10} iconClass="socialMediaIcons" uniqueID="herosm" />
       </section>
-      <section id="web-development">
+      <section id="web-development" className={styles.spaceTop}>
         <OrbittingText iconURL="/icons/globe.svg" textToRotate="WEB-DEVELOPMENT-" rotateSpeed={10} elementID="web-development-orbit" />
         <RotatingIcons elements={webStackIcons} centerSelector="#web-development-orbit" radiusVW={20} speed={10} iconClass="webDevOrbit" uniqueID="webdev" />
         <AccessibleHeading text="Web Development" level={1} />
@@ -56,9 +64,12 @@ export default async function Home() {
         <h2 className={`${styles.heading} ${silkscreen.className}`}>My Projects</h2>
         <ProjectsGrid projects={webProjects} elementID="web-projects-grid" />
       </section>
-      <section id="game-development">
+      <section id="game-development" className={styles.spaceTop}>
         <OrbittingText iconURL="/icons/game-controller.svg" textToRotate="GAME-DEVELOPMENT-" rotateSpeed={10} elementID="game-development-orbit" />
         <AccessibleHeading text="Game Development" level={1} />
+        <RotatingIcons elements={gameStackIcons} centerSelector="#game-development-orbit" radiusVW={20} speed={10} iconClass="gameDevOrbit" uniqueID="gamedev" />
+        <h2 className={`${styles.heading} ${silkscreen.className}`}>My Skills</h2>
+        <TechStack dataset={gameStack} />
       </section>
     </main>
   );
