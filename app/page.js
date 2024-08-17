7c3f1855a3dc732f20199fc5fc4c965ad19fa200
AdditionalRAM <taha@additionalram.com>
2024-08-17 09:18:57 +0300

    Added about me section

--- app/page.js
+++ app/page.js
@@ -61,7 +61,20 @@ export default async function Home() {
         <GlitchyText text="AdditionalRAM" fontClassName={silkscreen.className} />
         <GlitchyText text="Web and Game Developer" fontClassName={silkscreen.className} extraClassName={styles.bottomTitle} />
       </section>
-      <section id="web-development" className={styles.spaceTop}>
+      <section id="about">
+        <h2 className={`${styles.heading} ${silkscreen.className}`}>About Me</h2>
+        <div className={styles.paragraphHolder}>
+          <p className={styles.paragraph}>Hey there! I'm Taha.</p>
+          <p className={styles.paragraph}>I'm a self-taught web and game developer based in Berlin, Germany.</p>
+          <p className={styles.paragraph}>I've been actively learning by myself since 2018, but my love for programming started all the way back in primary school.</p>
+          <p className={styles.paragraph}>Recently, I've had the incredible opportunity to intern at Food for Thought Media & Mad About Pandas Game Dev Studios.</p>
+          <p className={styles.paragraph}>During this internship, I was able to demonstrate my skills in game development with state machines in Unity and C# and learn a lot about the game design process.</p>
+          <p className={styles.paragraph}>It was also there that I discovered my passion for web development after working on a web-based game.</p>
+          <p className={styles.paragraph}>This internship is not the only thing I've been doing. Scroll down to see what technologies I've learned by myself and the projects I've put them to use in!</p>
+
+        </div>
+      </section>
+      <section id="web-development">
         <OrbittingText iconURL="/icons/globe.svg" textToRotate="WEB-DEVELOPMENT-" rotateSpeed={10} elementID="web-development-orbit" />
         <RotatingIcons elements={webStackIcons} centerSelector="#web-development-orbit" radiusVW={20} speed={10} iconClass="webDevOrbit" uniqueID="webdev" />
         <AccessibleHeading text="Web Development" level={1} />
