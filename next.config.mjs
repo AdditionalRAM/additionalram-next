5eb2e6c0d1be240029fb2f56e3e83d825dbdf21e
AdditionalRAM <taha@additionalram.com>
2024-08-21 09:53:13 +0300

    Updated version for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "additionalram-next.vercel.app",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 }
