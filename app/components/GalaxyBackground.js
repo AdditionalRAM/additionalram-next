df2793b1e6c1973716c156c033b6d76f7b4d27ca
AdditionalRAM <taha@additionalram.com>
2024-08-21 09:31:04 +0300

    Added parallax scrolling to stars

--- app/components/GalaxyBackground.js
+++ app/components/GalaxyBackground.js
@@ -7,10 +7,18 @@ export default function GalaxyBackground() {
   const [starLayers, setStarLayers] = useState([]);
 
   useEffect(() => {
+    const handleScroll = () => {
+      let starLayers = document.querySelectorAll(`.${styles.starsLayer1}, .${styles.starsLayer2}, .${styles.starsLayer3}`);
+      let scrollY = window.scrollY;
+      starLayers.forEach((layer, i) => {
+        layer.style.transform = `translateY(${scrollY * (i + 1) * -0.1}px)`;
+      });
+    };
+
     const createStars = () => {
       let maxX = document.body.clientWidth;
-      let maxY = document.body.clientHeight;
-      let starsPerLayer = Math.floor(Math.random() * 25) + 50;
+      let maxY = document.body.clientHeight + window.innerHeight * 3;
+      let starsPerLayer = Math.floor(Math.random() * 40) + 75;
       let layerAmount = 3;
 
       const newStarLayers = [];
@@ -33,23 +41,28 @@ export default function GalaxyBackground() {
 
     createStars();
 
-    window.addEventListener('resize', handleResize);
+    // window.addEventListener('resize', handleResize);
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      // window.removeEventListener('resize', handleResize);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
   return (
     <div className={styles.backgroundContainer}>
       {starLayers.map((layer, i) => (
-        <div key={i} className={styles[`starsLayer${i + 1}`]}>
-          {layer.map((star, j) => (
-            <div
+        <div key={i} className={styles[`starsLayer${i + 1}`]} 
+        style={{height: document.body.clientHeight, width: document.body.clientWidth}}
+        >
+          {layer.map((star, j) => {
+            let starSize = Math.random() * i + 2;
+            return (<div
               key={j}
               className={styles.star}
-              style={{ left: star.left, top: star.top }}
-            />
-          ))}
+              style={{ left: star.left, top: star.top, width: `${starSize}px`, height: `${starSize}px` }}
+            />);
+          })}
         </div>
       ))}
     </div>
